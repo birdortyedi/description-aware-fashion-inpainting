0d4811ab26f7767e142a8f68044b441ba3548a21
birdortyedi <furkan.kinli@ozu.edu.tr>
2019-12-21 22:00:05 +0300

    model trials.

--- losses.py
+++ losses.py
@@ -45,14 +45,14 @@ class CustomInpaintingLoss(nn.Module):
     def __init__(self):
         super(CustomInpaintingLoss, self).__init__()
         self.content_loss = ContentLoss()  # (x, out) := (the original image, inpainting)
-        self.content_weight = 1.0
+        self.content_weight = 20.0
         self.style_loss = StyleLoss()  # (x, out) := (the original image, inpainting)
-        self.style_weight = 3.0
+        self.style_weight = 100.0
         self.structural_loss = pytorch_msssim.MSSSIM()  # (x, out) := (the original image, inpainting)
-        self.structural_weight = 3.0
+        self.structural_weight = 5.0
         self.adversarial_loss = AdverserialLoss()
         # (d_x, d_out) := (discriminator(x), discriminator(out))
-        self.adversarial_weight = 1.0
+        self.adversarial_weight = 0.5
 
     def forward(self, x, out, d_x, d_out):
         con_loss = self.content_loss(x, out.detach())
