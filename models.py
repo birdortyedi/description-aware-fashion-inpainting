3f7e9c31d15ccb43cd6f725cb89a5bd6949d4b4f
birdortyedi <furkan.kinli@ozu.edu.tr>
2019-12-31 15:59:19 +0300

    refine net implementation

--- models.py
+++ models.py
@@ -306,10 +306,10 @@ class RefineNet(nn.Module):
         self._1x1conv_8 = self._1x1conv_lrelu_block(in_channels=256, out_channels=64)
 
         self.block_9 = self._upsampling_in_lrelu_block(in_channels=64, out_channels=64)
-        self._1x1conv_9_2 = self._1x1conv_lrelu_block(in_channels=128, out_channels=32)
+        self._1x1conv_9 = self._1x1conv_lrelu_block(in_channels=128, out_channels=32)
 
         self.block_10 = self._upsampling_in_lrelu_block(in_channels=32, out_channels=32)
-        self._1x1conv_10_2 = self._1x1conv_lrelu_block(in_channels=32, out_channels=16)
+        self._1x1conv_10 = self._1x1conv_lrelu_block(in_channels=32, out_channels=16)
 
         self.block_11 = self._upsampling_tanh_block(in_channels=16, out_channels=3)
 
