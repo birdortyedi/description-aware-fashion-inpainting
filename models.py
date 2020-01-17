b9a4ceee50e14f55a2f866fa22bbee6b73212ffe
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 18:12:45 +0300

    losses are updated

--- models.py
+++ models.py
@@ -168,26 +168,37 @@ class CoarseNet(Net):
     def forward(self, x, descriptions, mask):
         x_1, m_1 = self.block_1(x, mask)
         x_1 = F.leaky_relu(self.in_1(x_1))
+        print(x_1.size())
         x_2, m_2 = self.block_2(x_1, m_1)
         x_2 = self.dropout(F.leaky_relu(self.in_2(x_2)))
+        print(x_2.size())
         x_3, m_3 = self.block_3(x_2, m_2)
         x_3 = self.dropout(F.leaky_relu(self.in_3(x_3)))
+        print(x_3.size())
 
         dil_res_x_3 = self.dilated_res_blocks(x_3)
         attention_map, _ = self.self_attention(dil_res_x_3)
 
         x_4, m_4 = self.block_4(x_3, m_3)
         x_4 = self.dropout(F.leaky_relu(self.in_4(x_4)))
+        print(x_4.size())
         x_5, m_5 = self.block_5(x_4, m_4)
         x_5 = self.dropout(F.leaky_relu(self.in_5(x_5)))
+        print(x_5.size())
 
         visual_embedding = self.avg_pooling(x_5).squeeze()
+        print(visual_embedding.size())
         textual_embedding = self.lstm_block(descriptions)
+        print(textual_embedding.size())
         embedding = torch.cat((visual_embedding, textual_embedding), dim=1)
+        print(embedding.size())
 
         x_6 = self.block_6(embedding.view(-1, 16, 4, 4))
+        print(x_6.size())
         x_6 = torch.cat((x_5, x_6), dim=1)
+        print(x_6.size())
         x_6 = self.dropout(self._1x1conv_6(x_6))
+        print(x_6.size())
 
         x_7 = self.block_7(x_6)
         x_7 = torch.cat((x_4, x_7), dim=1)
