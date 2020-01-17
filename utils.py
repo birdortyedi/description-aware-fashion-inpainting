18feb7d6684c0a064f9938c9a24fe6aaca8a6310
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 19:05:59 +0300

    losses are updated

--- utils.py
+++ utils.py
@@ -174,8 +174,8 @@ def weights_init(m):
         if m.bias is not None:
             nn.init.constant(m.bias, 0)
     elif classname.find('BatchNorm') != -1 or classname.find('InstanceNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        nn.init.normal_(m.weight, 1.0, 0.02)
+        nn.init.constant_(m.bias, 0)
 
 
 if __name__ == '__main__':
