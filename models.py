528a2cf1a049fae4ae231300f2d87694b46bb09a
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 23:17:00 +0300

    bug fix

--- models.py
+++ models.py
@@ -157,23 +157,23 @@ class CoarseNet(nn.Module):
         self.upsample = nn.Upsample(mode="nearest", scale_factor=2.0)
 
         # Decoder
-        self.block_7 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=1, padding=1,
+        self.block_7 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_7 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_8 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=1, padding=0,
+        self.block_8 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_8 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_9 = PartialConv2d(in_channels=192, out_channels=128, kernel_size=1, padding=0,
+        self.block_9 = PartialConv2d(in_channels=192, out_channels=128, kernel_size=3, padding=1,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_9 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_10 = PartialConv2d(in_channels=384, out_channels=128, kernel_size=1, padding=0,
+        self.block_10 = PartialConv2d(in_channels=384, out_channels=128, kernel_size=3, padding=1,
                                       bias=False, return_mask=True, multi_channel=True)
         self.in_10 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_11 = PartialConv2d(in_channels=192, out_channels=32, kernel_size=1, padding=0,
+        self.block_11 = PartialConv2d(in_channels=192, out_channels=32, kernel_size=3, padding=1,
                                       bias=False, return_mask=True, multi_channel=True)
         self.in_11 = nn.InstanceNorm2d(num_features=32, affine=True)
 
