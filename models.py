387095d63c8338d5b87d5e0fa49533080003f903
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 22:48:56 +0300

    bug fix

--- models.py
+++ models.py
@@ -69,46 +69,7 @@ class GlobalDiscriminator(nn.Module):
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        # Encoder
-        self.block_1 = PartialConv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3,
-                                     bias=False, return_mask=True, multi_channel=True)
 
-        self.block_2 = PartialConv2d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=2,
-                                     bias=False, return_mask=True, multi_channel=True)
-        self.in_2 = nn.InstanceNorm2d(num_features=64, affine=True)
-
-        self.block_3 = PartialConv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2,
-                                     bias=False, return_mask=True, multi_channel=True)
-        self.in_3 = nn.InstanceNorm2d(num_features=128, affine=True)
-
-        # Dilated Residual Blocks
-        self.dilated_res_blocks = dilated_res_blocks(num_features=128, kernel_size=5, padding=4)
-        # Self-Attention
-        self.self_attention = SelfAttention(in_channels=128)
-
-        # Visual features for concatenating with textual features
-        self.block_4 = PartialConv2d(in_channels=128, out_channels=64, kernel_size=5, stride=2, padding=2,
-                                     bias=False, return_mask=True, multi_channel=True)
-        self.in_4 = nn.InstanceNorm2d(num_features=64, affine=True)
-
-        self.avg_pooling = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.dropout = nn.Dropout2d(p=0.3)
-
-        self.upsample = nn.Upsample(mode="nearest", scale_factor=2.0)
-        self.p_conv_9 = PartialConv2d(in_channels=192, out_channels=64, kernel_size=1, padding=0,
-                                      bias=False, return_mask=True, multi_channel=True)
-        self.in_9 = nn.InstanceNorm2d(num_features=64, affine=True)
-
-        self.p_conv_10 = PartialConv2d(in_channels=320, out_channels=128, kernel_size=1, padding=0,
-                                      bias=False, return_mask=True, multi_channel=True)
-        self.in_10 = nn.InstanceNorm2d(num_features=128, affine=True)
-
-        self.p_conv_11 = PartialConv2d(in_channels=192, out_channels=32, kernel_size=1, padding=0,
-                                       bias=False, return_mask=True, multi_channel=True)
-        self.in_11 = nn.InstanceNorm2d(num_features=32, affine=True)
-
-        self.p_conv_12 = PartialConv2d(in_channels=67, out_channels=3, kernel_size=1, bias=False, return_mask=True, multi_channel=True)
 
 
 class RefineNet(Net):
@@ -163,25 +124,66 @@ class CoarseNet(Net):
     def __init__(self, vocab_size):
         super(CoarseNet, self).__init__()
 
+        super(Net, self).__init__()
+        # Encoder
+        self.block_1 = PartialConv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3,
+                                     bias=False, return_mask=True, multi_channel=True)
+
+        self.block_2 = PartialConv2d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=2,
+                                     bias=False, return_mask=True, multi_channel=True)
+        self.in_2 = nn.InstanceNorm2d(num_features=64, affine=True)
+
+        self.block_3 = PartialConv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2,
+                                     bias=False, return_mask=True, multi_channel=True)
+        self.in_3 = nn.InstanceNorm2d(num_features=128, affine=True)
+
+        # Dilated Residual Blocks
+        self.dilated_res_blocks = dilated_res_blocks(num_features=128, kernel_size=5, padding=4)
+        # Self-Attention
+        self.self_attention = SelfAttention(in_channels=128)
+
+        # Visual features for concatenating with textual features
+        self.block_4 = PartialConv2d(in_channels=128, out_channels=64, kernel_size=5, stride=2, padding=2,
+                                     bias=False, return_mask=True, multi_channel=True)
+        self.in_4 = nn.InstanceNorm2d(num_features=64, affine=True)
+
         self.block_5 = PartialConv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_5 = nn.InstanceNorm2d(num_features=128)
 
-        self.block_6 = PartialConv2d(in_channels=128, out_channels=512, kernel_size=5, stride=2, padding=2,
+        self.block_6 = PartialConv2d(in_channels=128, out_channels=1024, kernel_size=5, stride=2, padding=2,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_6 = nn.InstanceNorm2d(num_features=128)
 
         # LSTM
-        self.lstm_block = lstm_block(vocab_size, output_size=512)
+        self.lstm_block = lstm_block(vocab_size, output_size=1024)
+
+        self.avg_pooling = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.dropout = nn.Dropout2d(p=0.3)
+        self.upsample = nn.Upsample(mode="nearest", scale_factor=2.0)
 
         # Decoder
-        self.p_conv_7 = PartialConv2d(in_channels=16, out_channels=32, kernel_size=1, padding=0,
-                                      bias=False, return_mask=True, multi_channel=True)
+        self.block_7 = PartialConv2d(in_channels=16, out_channels=32, kernel_size=1, padding=0,
+                                     bias=False, return_mask=True, multi_channel=True)
         self.in_7 = nn.InstanceNorm2d(num_features=32, affine=True)
 
-        self.p_conv_8 = PartialConv2d(in_channels=160, out_channels=128, kernel_size=1, padding=0,
-                                      bias=False, return_mask=True, multi_channel=True)
+        self.block_8 = PartialConv2d(in_channels=160, out_channels=128, kernel_size=1, padding=0,
+                                     bias=False, return_mask=True, multi_channel=True)
         self.in_8 = nn.InstanceNorm2d(num_features=128, affine=True)
+
+        self.block_9 = PartialConv2d(in_channels=192, out_channels=64, kernel_size=1, padding=0,
+                                     bias=False, return_mask=True, multi_channel=True)
+        self.in_9 = nn.InstanceNorm2d(num_features=64, affine=True)
+
+        self.block_10 = PartialConv2d(in_channels=320, out_channels=128, kernel_size=1, padding=0,
+                                      bias=False, return_mask=True, multi_channel=True)
+        self.in_10 = nn.InstanceNorm2d(num_features=128, affine=True)
+
+        self.block_11 = PartialConv2d(in_channels=192, out_channels=32, kernel_size=1, padding=0,
+                                      bias=False, return_mask=True, multi_channel=True)
+        self.in_11 = nn.InstanceNorm2d(num_features=32, affine=True)
+
+        self.block_12 = PartialConv2d(in_channels=67, out_channels=3, kernel_size=1, bias=False, return_mask=True, multi_channel=True)
 
     def forward(self, x, descriptions, mask):
         x_1, m_1 = self.block_1(x, mask)
@@ -212,33 +214,33 @@ class CoarseNet(Net):
         x_7 = self.upsample(o)
         print(x_7.size())
         print(m_5.size())
-        x_7, m_7 = self.p_conv_7(x_7, m_5)
+        x_7, m_7 = self.block_7(x_7, m_5)
         x_7 = F.leaky_relu(self.in_7(x_7), negative_slope=0.2)
         x_7 = self.dropout(torch.cat((x_5, x_7), dim=1))
 
         x_8 = self.upsample(x_7)
-        x_8, m_8 = self.p_conv_8(x_8, m_7)
+        x_8, m_8 = self.block_8(x_8, m_7)
         x_8 = F.leaky_relu(self.in_8(x_8), negative_slope=0.2)
         x_8 = self.dropout(torch.cat((x_4, x_8), dim=1))
 
         x_9 = self.upsample(x_8)
-        x_9, m_9 = self.p_conv_9(x_9, m_8)
+        x_9, m_9 = self.block_9(x_9, m_8)
         x_9 = F.leaky_relu(self.in_9(x_9), negative_slope=0.2)
         x_9 = self.dropout(torch.cat((x_3, x_9, attention_map), dim=1))
 
         x_10 = self.upsample(x_9)
-        x_10, m_10 = self.p_conv_10(x_10, m_9)
+        x_10, m_10 = self.block_10(x_10, m_9)
         x_10 = F.leaky_relu(self.in_10(x_10), negative_slope=0.2)
         x_10 = self.dropout(torch.cat((x_2, x_10), dim=1))
 
         x_11 = self.upsample(x_10)
-        x_11, m_11 = self.p_conv_11(x_11, m_10)
+        x_11, m_11 = self.block_11(x_11, m_10)
         x_11 = F.leaky_relu(self.in_11(x_11), negative_slope=0.2)
         x_11 = torch.cat((x_1, x_11), dim=1)
 
         x_12 = self.upsample(x_11)
         x_12 = torch.cat((x, x_12), dim=1)
-        x_12, _ = self.p_conv_12(x_12, m_11)
+        x_12, _ = self.block_12(x_12, m_11)
         x_12 = F.tanh(x_12)
 
         return x_12
