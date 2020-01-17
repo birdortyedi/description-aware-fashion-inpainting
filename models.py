4af89222a5d001a170c0631c1af6a5458f42f1ca
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 23:08:06 +0300

    bug fix

--- models.py
+++ models.py
@@ -161,7 +161,7 @@ class CoarseNet(nn.Module):
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_7 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_8 = PartialConv2d(in_channels=256, out_channels=128, kernel_size=1, padding=0,
+        self.block_8 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=1, padding=0,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_8 = nn.InstanceNorm2d(num_features=128, affine=True)
 
