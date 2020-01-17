91a301ecaac3e8305c8b4ba0a360c2977d1564df
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-17 23:10:26 +0300

    bug fix

--- models.py
+++ models.py
@@ -161,7 +161,7 @@ class CoarseNet(nn.Module):
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_7 = nn.InstanceNorm2d(num_features=128, affine=True)
 
-        self.block_8 = PartialConv2d(in_channels=128, out_channels=128, kernel_size=1, padding=0,
+        self.block_8 = PartialConv2d(in_channels=256, out_channels=128, kernel_size=1, padding=0,
                                      bias=False, return_mask=True, multi_channel=True)
         self.in_8 = nn.InstanceNorm2d(num_features=128, affine=True)
 
@@ -208,6 +208,8 @@ class CoarseNet(nn.Module):
         print(x_7.size())
 
         x_8 = self.upsample(x_7)
+        print(x_8.size())
+        print(m_7.size())
         x_8, m_8 = self.block_8(x_8, m_7)
         x_8 = F.leaky_relu(self.in_8(x_8), negative_slope=0.2)
         x_8 = self.dropout(torch.cat((x_4, x_8), dim=1))
