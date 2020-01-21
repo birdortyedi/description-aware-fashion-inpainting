b66e8caccf92f9f373b274b28e02d4e00bfbaf81
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-21 17:13:34 +0300

    lr decay bug fix

--- losses.py
+++ losses.py
@@ -169,7 +169,7 @@ class CustomLoss(nn.Module):
 
         tv_loss = self.tv(composite)
 
-        return pixel_valid_loss + 6.0 * pixel_hole_loss + 0.5 * content_loss + 120.0 * style_loss + 0.1 * tv_loss, \
+        return 10.0 * pixel_valid_loss + 120.0 * pixel_hole_loss + 1.0 * content_loss + 50.0 * style_loss + 0.1 * tv_loss, \
             pixel_valid_loss, pixel_hole_loss, content_loss, style_loss, tv_loss
 
     @staticmethod
