0908e595d0fdd50c0db55b26efc8902a9181e6e2
birdortyedi <furkan.kinli@ozu.edu.tr>
2020-01-31 03:52:06 +0300

    gaussian noise to latent vector

--- main.py
+++ main.py
@@ -75,19 +75,19 @@ writer = SummaryWriter()
 
 
 def train(epoch, img_loader, mask_loader):
-    for batch_idx, (y_train, x_desc) in tqdm(enumerate(img_loader), ncols=50, desc="Training",
+    for batch_idx, (y_train, x_desc, x_train, x_mask) in tqdm(enumerate(img_loader), ncols=50, desc="Training",
                                              bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)):
-        x_mask, _ = next(iter(mask_loader))
-        if x_mask.size(0) != y_train.size(0):
-            x_mask = x_mask[:y_train.size(0)]
+        # x_mask, _ = next(iter(mask_loader))
+        # if x_mask.size(0) != y_train.size(0):
+        #    x_mask = x_mask[:y_train.size(0)]
         num_step = epoch * len(img_loader) + batch_idx
-        # x_train = x_train.float().to(device)
+        x_train = x_train.float().to(device)
         x_desc = x_desc.long().to(device)
         x_mask = x_mask.float().to(device)
         # x_local = x_local.float().to(device)
         y_train = y_train.float().to(device)
 
-        x_train = x_mask * y_train + (1.0 - x_mask) * 0.5
+        # x_train = x_mask * y_train + (1.0 - x_mask) * 0.5
         noise = torch.zeros((x_mask.size(0), 256), dtype=torch.float32).normal_().to(device)
 
         net.zero_grad()
